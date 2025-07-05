bf822c2b2c8f8f572da843db242a070447de3566
Tiji <sjunesuh@gmail.com>
2025-07-05 17:07:54 +0800

    Fix broken button due to changes in draw method and match design with other widgets

--- src/main/java/com/tiji/elements/display/ui/widget/Button.java
+++ src/main/java/com/tiji/elements/display/ui/widget/Button.java
@@ -5,26 +5,19 @@ import com.tiji.elements.core.Position;
 import com.tiji.elements.display.DrawCalls;
 
 public class Button extends Widget {
-    private final int[] elements;
-    private final int width;
-    private boolean isHovered;
+    private final int textId;
+    private final Box container;
 
     private static final int BUTTON_HEIGHT = 30;
-    private static final int HORIZ_PADDING = 10;
     private static final int VERT_PADDING = 5;
 
-    private static final Color DEFAULT_COLOR = new Color(200, 200, 200);
-    private static final Color HOVER_COLOR = new Color(255, 255, 255);
-    private static final Color TEXT_COLOR = new Color(0, 0, 0);
+    private static final Color TEXT_COLOR = new Color(255, 255, 255);
 
     public Button(String text, Position pos, int width) {
         super(pos);
-        this.elements = new int[2];
-        this.width = width;
-        this.isHovered = false;
 
-        elements[0] = DrawCalls.rectangle(getPos(), width+HORIZ_PADDING*2, BUTTON_HEIGHT, DEFAULT_COLOR);
-        elements[1] = DrawCalls.text(getPos().translate((int) DrawCalls.textCenter(text, width) + HORIZ_PADDING, VERT_PADDING), text, TEXT_COLOR);
+        textId = DrawCalls.text(getPos().translate((int) DrawCalls.textCenter(text, width), VERT_PADDING), text, TEXT_COLOR);
+        container = new Box(pos, width, BUTTON_HEIGHT);
     }
 
     public Button(String text, Position pos) {
@@ -32,16 +25,11 @@ public class Button extends Widget {
     }
 
     @Override
-    public void draw(Position mousePos) {
-        if (isPointInside(mousePos, width, BUTTON_HEIGHT) != isHovered) {
-            DrawCalls.forget(elements[0]);
-            isHovered = !isHovered;
-            elements[0] = DrawCalls.rectangle(getPos(), width+HORIZ_PADDING*2, BUTTON_HEIGHT, isHovered ? HOVER_COLOR : DEFAULT_COLOR);
-        }
-    }
+    public void draw(Position mousePos) {}
 
     @Override
     public void remove() {
-        for (int i : elements) DrawCalls.forget(i);
+        DrawCalls.forget(textId);
+        container.remove();
     }
 }
