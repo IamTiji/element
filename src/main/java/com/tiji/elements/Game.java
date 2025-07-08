40cc6a5fd34d99c3103e174899e76480eb2ebbfb
Tiji <sjunesuh@gmail.com>
2025-07-08 18:27:49 +0800

    Add Java version requirement

--- src/main/java/com/tiji/elements/Game.java
+++ src/main/java/com/tiji/elements/Game.java
@@ -32,8 +32,13 @@ public class Game {
     public static final int HEIGHT = 180;
     public static final int TARGET_MSPT = 1000 / 60;
     public static final int THREAD_COUNT = 8;
+    public static final int MINIMUM_JAVA_VERSION = 21;
 
     public static void main(String[] args) {
+        if (Integer.parseInt(System.getProperties().get("java.version").toString().split("\\.")[0]) < MINIMUM_JAVA_VERSION) {
+            throw new RuntimeException("Java version must be at least " + MINIMUM_JAVA_VERSION + ". Current version: " + System.getProperties().get("java.version"));
+        };
+
         settingHandler.loadSettings();
         Language language = (Language) settingHandler.getSetting("language");
         translationHandler.loadTranslations(language);
